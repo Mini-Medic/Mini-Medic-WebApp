938fecf71d5371d95911a07b26e1bd2b31e6ae6e
tianhuizhou <tianhuizhou@users.noreply.github.com>
2021-03-21 03:23:49 -1000

    Revert "Merge branch 'main' of https://github.com/Mini-Medic/Mini-Medic-WebApp into main"
    This reverts commit a5a9bd7b655bb078dfc82a7b3a99809a04511f37, reversing
    changes made to f73d468805d7ca69e3b3d038b1e25517864fbe6c.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react'
 import { Footer, Header } from './components/common';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 import './App.css';
 
 
 function App() {
   return (
-    <>
-        <Router>
-            <Header />
-            <Switch>
-                <Route path={'/'} exact component/>
-            </Switch>
-            <Footer />
-        </Router>
-    </>
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
   );
 }
 
