383dc9ae4323d81c327a357343a18bd11135564f
tianhuizhou <tianhuizhou@users.noreply.github.com>
2021-03-21 21:10:44 -1000

    update footer

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,47 +1,57 @@
 import React from 'react';
-
-import './Footer.css';
-
-function Footer () {
-
+import './Footer.css'
+function Footer() {
   return (
-    <section className="footer">
-      <hr className="footer-seperator" />
-      <section className="footer-info">
-        <section className="footer-info-left">
-          <section className="footer-info__name">
-              Mini Medic-Oahu
-          </section>
-          <section className="footer-info__returns">
-            Returns Policy
-            <br />
-            Delivery
-          </section>        
+      <div className={'footer_container'}>
+        <div className={'footer_links'}>
+          <div className={'footer_links-wrapper'}>
+            <div className={'footer_links-items'}>
+              <h2>About us</h2>
+              <a href="/">balabala</a>
+              <a href="/">balabala</a>
+              <a href="/">balabala</a>
+              <a href="/">balabala</a>
+            </div>
+            <div className={'footer_links-items'}>
+              <h2>About us</h2>
+              <a href="/">balabala</a>
+              <a href="/">balabala</a>
+              <a href="/">balabala</a>
+              <a href="/">balabala</a>
+            </div>
+          </div>
+          <div className={'footer_links-wrapper'}>
+            <div className={'footer_links-items'}>
+              <h2>About us</h2>
+              <a href="/">balabala</a>
+              <a href="/">balabala</a>
+              <a href="/">balabala</a>
+              <a href="/">balabala</a>
+            </div>
+            <div className={'footer_links-items'}>
+              <h2>About us</h2>
+              <a href="/">balabala</a>
+              <a href="/">balabala</a>
+              <a href="/">balabala</a>
+              <a href="/">balabala</a>
+            </div>
+          </div>
+        </div>
+        <section className="social_media">
+          <div className="social_media-wrap">
+            <div className="footer_logo">
+              <a href="/" id="footer_logo">Mini Medic</a>
+            </div>
+            <p className="website_rights">Mini Medic 2021. All rights reserved</p>
+            <div className="social_icons">
+              <a href="/" className="social_icon-link"><i className="fab fa-twitter"/></a>
+              <a href="/" className="social_icon-link"><i className="fab fa-facebook"/></a>
+              <a href="/" className="social_icon-link"><i className="fab fa-instagram"/></a>
+              <a href="/" className="social_icon-link"><i className="fab fa-linkedin"/></a>
+            </div>
+          </div>
         </section>
-        <section className="footer-info-center">
-          <section className="footer-info__email">
-            linweile@gmail.com
-          </section>
-          <section className="footer-info__terms">
-            Terms and Conditions
-            <br />
-            Copyright
-          </section>
-        </section>
-        <section className="footer-info-right">
-          <section className="footer-info__number">
-            Phone #: 0000000000
-          </section>
-          <section className="footer-info__contact">
-            <br />
-            Contact Us
-          </section>
-        </section>
-      </section>
-      <hr className="footer-seperator" />
-    </section>
-  )
-
+      </div>
+  );
 }
-
 export default Footer;
