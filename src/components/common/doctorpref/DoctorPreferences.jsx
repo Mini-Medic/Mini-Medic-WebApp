0a6e88cab31e4185cb11b60d6ab02ed8903b1631
Sim <sjavella@hawaii.edu>
2021-04-23 14:57:05 -1000

    Done with Doctor Pref page.

---
+++ src/components/common/doctorpref/DoctorPreferences.jsx
@@ -0,0 +1,60 @@
+import React, { } from 'react';
+import '../doctorpref/DoctorPreferences.css';
+import '../../../App.css';
+import { Button } from 'semantic-ui-react';
+
+function DoctorPreferences() {
+  return (
+        <div className='container'>
+          <div className='body'>
+            <h1>Doctor Preferences</h1>
+            <p>Please check the corresponding boxes of the doctor you would like to search for.</p>
+            <div className='container-list'>
+            <div className='doctorPref-container'>
+              <p>Gender</p>
+
+                <div className='doctorRadio'>
+                  <input type="radio" value="Male" name="gender" /> Male
+                  <input type="radio" value="Female" name="gender" /> Female
+                  <input type="radio" value="Other" name="gender" /> Other
+                </div>
+
+            </div>
+
+            <div className='doctorPref-container'>
+              <p>Experience</p>
+
+              <div className='doctorRadio'>
+                <select>
+                  <option value="0-10 Years">0-10 Years</option>
+                  <option value="10-20 Years">10-20 Years</option>
+                  <option value="20+ Years">20+ Years</option>
+                </select>
+              </div>
+
+            </div>
+
+            <div className='doctorPref-container'>
+              <p>Location</p>
+
+              <div className='doctorRadio'>
+                <select>
+                  <option value="Honolulu">Honolulu</option>
+                  <option value="Pearl City">Pearl City</option>
+                  <option value="Waianae">Waianae</option>
+                  <option value="Kahala">Kahala</option>
+                  <option value="Mililani">Mililani</option>
+                </select>
+              </div>
+            </div>
+            </div>
+
+            <div className='submit' >
+              <Button basic color='blue' className='submit-button' size='small'>Submit</Button>
+            </div>
+          </div>
+        </div>
+  )
+}
+
+export default DoctorPreferences;
