5c472ae5380526de0cfad5932ec01173ca06a04c
angeli-amascual <aamascua@hawaii.edu>
2021-04-30 18:05:56 -1000

    Font size adjustments, changed Illness Info box

--- src/components/pages/illness/IllnessInfo.jsx
+++ src/components/pages/illness/IllnessInfo.jsx
@@ -7,15 +7,15 @@ function IllnessInfo(props){
         <div className={'illnessInfo-popup'}>
             <div className={'illnessInfo-popup-inner'}>
             <div className={'illnessInfo-title'}>
-                <h1>{props.title}</h1>
+                <h1 style={{ fontSize: '32px' }}>{props.title}</h1>
             </div>
             <div className={'illnessInfo-content'}>
-              <h3>Symptoms:</h3>
-                <p>{props.symptoms}</p>
+              <h3 style={{ fontSize: '24px' }}>Symptoms:</h3>
+                <p style={{ fontSize: '18px' }}>{props.symptoms}</p>
             </div>
             <div className={'illnessInfo-content'}>
-              <h3>Description:</h3>
-                <p>{props.description}</p>
+              <h3 style={{ fontSize: '24px' }}>Description:</h3>
+                <p style={{ fontSize: '18x' }}>{props.description}</p>
             </div>
             <div className={'illnessInfo-footer'}>
                 <button className={'cancel'} onClick={props.hidecomponent}>Cancel</button>
